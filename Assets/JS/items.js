e42cf69b20bfae64fce2b2b315b133dcf560f169
Felix <alexg93x2@gmail.com>
2018-08-06 22:55:56 -0700

    Added a sell function

--- Assets/JS/items.js
+++ Assets/JS/items.js
@@ -14,5 +14,17 @@ function buyItem(item){
     playerCurrentTacos.textContent = playerTacos
 };
 
+function sellItem(item){
+    // CHECK IF player has the item
+    if(playerInventory[item]){
+        // if so, subtract 1 from qty
+        playerInventory[item] -= 1;
+    }
+    shopInventory[item] + 1;
+    var message = `Thanks for the ${item}!!`
+    shopMessage.innerHTML = message;
+    playerCurrentTacos.textContent = playerTacos
+};
+
 
 //abstraction
